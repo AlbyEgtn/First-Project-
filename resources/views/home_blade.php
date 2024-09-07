d88f63748cc623bfdbfd06943924b847740eeef9
AlbyEgtn <mukradsumrad@gmail.com>
2024-09-07 12:56:34 +0700

    first commit

---
+++ resources/views/home_blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <h1 class="position-absolute top-50 start-50 translatemiddle">Belajar Framework Laravel!</h1>
+    <scriptsrc="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></scriptsrc=>
+</body>
+</html>
